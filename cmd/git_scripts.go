6a99cfb954c8aa2f7f6b782a6d9f97d713b994f1
Eugen Eisler <eugen.eisler@siemens.com>
2023-06-05 16:29:40 +0200

    fix output dir for scripts

--- cmd/git_scripts.go
+++ cmd/git_scripts.go
@@ -102,7 +102,7 @@ func generateGitScripts() (err error) {
 	}
 	generator := &gen.Generator{
 		FileNameBuilder: &gen.DefaultsFileNameBuilder{
-			OutputPath: "", RelativeToTemplate: false, RelativeToData: true},
+			OutputPath: absOutDir, RelativeToTemplate: false, RelativeToData: true},
 		NextTemplateLoader:     templateProvider,
 		NextTemplateDataLoader: templateDataProvider,
 	}
