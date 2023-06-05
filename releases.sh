de838ced81d59e0b0d19e9d0350e15a873fe493a
Eugen Eisler <eugen.eisler@siemens.com>
2023-06-05 20:57:29 +0200

    add release job

--- releases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export RELEASES=${PWD}/RELEASES
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-extldflags=-static" -o "$RELEASES/linux/"
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-extldflags=-static" -o "$RELEASES/macos/"
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-extldflags=-static" -o "$RELEASES/windows/"
